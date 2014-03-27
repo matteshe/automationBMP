669e5178d1fcf7bf2566f7739580858793d674bd
d-biehl <daniel.biehl@imbus.de>
2014-03-27 07:39:30 +0100

    Revert ""
    This reverts commit 8d87aa213b4a8e131034e4ba617d044b434b43bb.

--- de.telekom.testframework/src/main/java/de/telekom/testframework/testng/TestNGReportHandler.java
+++ de.telekom.testframework/src/main/java/de/telekom/testframework/testng/TestNGReportHandler.java
@@ -18,8 +18,7 @@ import org.testng.Reporter;
 import org.testng.util.Strings;
 
 /**
- * The Report Handler
- * 
+ *
  * @author Daniel Biehl
  */
 class TestNGReportHandler implements ReportHandler {
