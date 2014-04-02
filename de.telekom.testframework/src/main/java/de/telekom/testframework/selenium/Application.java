35e826fb2b3a83c138b12a2dc07bfb0008026cf9
d-biehl <daniel.biehl@imbus.de>
2014-04-02 18:45:57 +0200

    set Application.newTab to deprecated

--- de.telekom.testframework/src/main/java/de/telekom/testframework/selenium/Application.java
+++ de.telekom.testframework/src/main/java/de/telekom/testframework/selenium/Application.java
@@ -91,7 +91,11 @@ public abstract class Application implements SearchContext, FieldSearchContextGe
 
     /**
      * create a new tab in the browser
+     * @deprecated there is no support for Tabs in Selenium, you cannot address Tabs uniquely 
+     * open a new window in the Browser is the same like open a new Tab
+     * use {@link de.telekom.testframework.selenium.Browser#newTab} instead
      */
+    @Deprecated
     public void openNewTab() {
         handle("open new tab", new Runnable() {
 
