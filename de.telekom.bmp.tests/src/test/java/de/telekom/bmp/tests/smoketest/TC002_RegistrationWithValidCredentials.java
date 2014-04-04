4681be4240269a484071946e0ac4564cbf8a59f9
htm <htm@192.168.0.103>
2014-04-04 14:57:01 +0200

    refactor. use injection for google account instead of object creation store user details in test data database

--- de.telekom.bmp.tests/src/test/java/de/telekom/bmp/tests/smoketest/TC002_RegistrationWithValidCredentials.java
+++ de.telekom.bmp.tests/src/test/java/de/telekom/bmp/tests/smoketest/TC002_RegistrationWithValidCredentials.java
@@ -4,11 +4,9 @@ import com.google.inject.Inject;
 import de.telekom.bmp.BmpApplication;
 import de.telekom.bmp.data.Datapool;
 import de.telekom.bmp.data.User;
-import de.telekom.bmp.pages.GoogleLoginPage;
-import de.telekom.bmp.pages.GoogleReadMailPage;
 import de.telekom.bmp.pages.Home;
 import de.telekom.bmp.pages.Signup;
-import de.telekom.bmp.pages.accountsetup.AccountActivation;
+import de.telekom.bmp.pages.accountsetup.AccountActivationPage;
 import de.telekom.bmp.tests.GoogleMailAccount;
 import static de.telekom.testframework.Actions.*;
 import de.telekom.testframework.annotations.QCId;
@@ -16,17 +14,18 @@ import de.telekom.testframework.selenium.Browser;
 import de.telekom.testframework.selenium.annotations.UseWebDriver;
 import java.util.Date;
 import static org.hamcrest.Matchers.notNullValue;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 @UseWebDriver
 @QCId("123456")
 public class TC002_RegistrationWithValidCredentials {
-    
+
     public static final String APP_DOMAIN = "testcloud.bmptest.de";
-    
+
     public static final String HTACCESS_CREDENTIALS = "toon:HullyGully";
-    
+
     @Inject
     BmpApplication app;
 
@@ -41,87 +40,80 @@ public class TC002_RegistrationWithValidCredentials {
 
     // Needed user
     User user;
-    
+
     @Inject
     Browser browser;
-        
+
     @Inject
-    AccountActivation accountActivation;
-    
+    AccountActivationPage accountActivation;
+
     @Inject
-    GoogleLoginPage loginPage;
-    
-    @Inject
-    GoogleReadMailPage readMailPage;
-    
+    GoogleMailAccount mailAccount;
+
     @BeforeTest
     public void setup() {
         // create a valid and not registered user
-        user = createUser();        
+        user = createUser();
         assertThat(user, notNullValue());
-        
+
         navigateTo(app);
+    }
+
+    @AfterTest
+    public void tearDown() {
+        datapool.save(user);
     }
 
     @Test
     public void test_RegistrationWithValidCredentials() throws InterruptedException {
-       
-        try {
-            
-// WORKAROUND BECAUSE OF CMS
-            navigateTo(signup);
-            
-            click(homePage.registerBtn);            
-            
-            set(signup.emailAddress,user.email);
-            
-            assertThat("signup.iconValid.isDisplayed", signup.iconValid.isDisplayed());
-            
-            Thread.sleep(1000);
-            
-            // button to register
-            click(signup.signup);
-            
-            
-            
-            user.registered = registerUser();
-        } finally {            
-            datapool.save(user);
-        }
+        navigateTo(signup);
+
+        click(homePage.registerBtn);
+
+        set(signup.emailAddress, user.email);
+
+        assertThat("signup.iconValid.isDisplayed", signup.iconValid.isDisplayed());
+
+        Thread.sleep(1000);
+
+        // button to register
+        click(signup.signup);
+
+        user.registered = registerUser();
     }
 
     private boolean registerUser() throws InterruptedException {
-        GoogleMailAccount mailAccount = new GoogleMailAccount(user.email, GoogleMailAccount.MAIL_PASSWORD)
-                .browser(browser)
-                .loginPage(loginPage)
-                .readMailPage(readMailPage);
-        
+        mailAccount.setUsername(user.email);
+        mailAccount.setPassword(GoogleMailAccount.MAIL_PASSWORD);
+
         String confirmLink = mailAccount.checkGoogleMailAccountAndExtractConfirmLink();
         assertThat(confirmLink, !confirmLink.equals(""));
         confirmLink = addHtaccessCredentials(confirmLink);
-        
+
         browser.navigate().to(confirmLink);
         fillActivationForm();
         Thread.sleep(500);
         return true;
     }
-    
+
     private void fillActivationForm() {
         set(accountActivation.firstName, "max");
-        set(accountActivation.lastName, "mustermann" + 12345623);
+        set(accountActivation.lastName, user.name);
         set(accountActivation.companyName, "nicht Telekom");
-        set(accountActivation.password, "12345!QAY");
-        set(accountActivation.confirmPassword, "12345!QAY");        
-        click(accountActivation.termsAndCondition);        
+        set(accountActivation.password, user.password);
+        set(accountActivation.confirmPassword, "12345!QAY");
+        click(accountActivation.termsAndCondition);
         click(accountActivation.createAccountBtn);
     }
 
     private User createUser() {
         User newUser = new User();
         newUser.email = createMailAlias();
+        newUser.password = "12345!QAY";
+        newUser.name = "mustermann";
         newUser.registered = false;
         newUser.valid = true;
-        
+
         return newUser;
     }
 
