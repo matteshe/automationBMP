c315812cc513171b024e78ad11264117a8730f46
matteshe <mathias.herkt@sqs.com>
2014-04-07 17:00:54 +0200

    add new locator of feedback info message

--- de.telekom.bmp/src/main/java/de/telekom/bmp/pages/Home.java
+++ de.telekom.bmp/src/main/java/de/telekom/bmp/pages/Home.java
@@ -4,6 +4,7 @@ import de.telekom.bmp.BmpApplication;
 import de.telekom.testframework.selenium.Page;
 import de.telekom.testframework.selenium.annotations.Path;
 import de.telekom.testframework.selenium.controls.Button;
+import de.telekom.testframework.selenium.controls.Text;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import org.openqa.selenium.support.FindBy;
@@ -25,4 +26,6 @@ public class Home extends Page {
     @FindBy(xpath = "//a[@href='./signup' or contains(text(),'Registrieren')]")    
     public Button registerBtn;
     
+    @FindBy(xpath = "//li[@class='feedbackPanelINFO']")
+    public Text feedbackMessage;
 }
