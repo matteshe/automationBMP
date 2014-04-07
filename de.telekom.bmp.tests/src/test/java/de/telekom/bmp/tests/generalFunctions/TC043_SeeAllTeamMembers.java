d4f8cf2d6a39f2f6f1e98bfa2cdb598240d4afbd
tsarmiento <t.sarmiento@external.telekom.de>
2014-04-07 17:05:51 +0200

    New TCs under General Functions

---
+++ de.telekom.bmp.tests/src/test/java/de/telekom/bmp/tests/generalFunctions/TC043_SeeAllTeamMembers.java
@@ -0,0 +1,76 @@
+
+package de.telekom.bmp.tests.generalFunctions;
+
+
+import de.telekom.bmp.data.Datapool;
+
+import de.telekom.bmp.functional.FunctionalActions;
+import de.telekom.bmp.pages.Header;
+import de.telekom.bmp.pages.account.Dashboard;
+import de.telekom.bmp.pages.settings.Directories;
+
+import static de.telekom.testframework.Actions.*;
+import static de.telekom.testframework.reporting.Reporter.reportMessage;
+
+import de.telekom.testframework.selenium.annotations.UseWebDriver;
+import javax.inject.Inject;
+import static org.hamcrest.Matchers.is;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Tatiana
+ */
+
+@UseWebDriver
+public class TC043_SeeAllTeamMembers {
+    
+    @Inject
+    FunctionalActions functional;
+    
+    @Inject 
+    Datapool datapool;
+    
+          
+    @Inject 
+    Dashboard dashboard;
+    
+    @Inject 
+    Directories directories;
+    
+    
+        
+    @Test
+    public void test1() throws InterruptedException {
+        
+       functional.login("sysadmin4.0@test.com", "password");
+       
+       //createOrder.navigateTo("/1262");
+             
+
+//        User user = datapool.users()             
+//                .field("valid").equal(true)
+//                .field("registered").equal(true)
+//                .field("role").equal(UserRole.USER).get();
+//
+//        assertThat("we have a valid user", user != null);
+//
+//        functional.login(user.email, user.password);
+
+        
+       navigateTo(dashboard);
+       click(dashboard.allMembersLnk);
+       
+       verifyThat(directories.usersTable.isDisplayed());
+       int teamMembers = 0;
+       
+       for (WebElement member: directories.teamMembers) {
+           teamMembers++;
+           reportMessage(member.getText());
+       }
+       
+       verifyThat(teamMembers, is(8));
+              
+    }
+}
