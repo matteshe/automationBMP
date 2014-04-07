9a6a99d1f76f8add3bdab638063a60a8720f4251
matteshe <mathias.herkt@sqs.com>
2014-04-07 12:06:33 +0200

    change constant usage to meth signature

--- de.telekom.bmp.tests/src/test/java/de/telekom/bmp/tests/tcs_04_loginAndRegistration/TC10_Login_forgotten.java
+++ de.telekom.bmp.tests/src/test/java/de/telekom/bmp/tests/tcs_04_loginAndRegistration/TC10_Login_forgotten.java
@@ -2,18 +2,19 @@
 package de.telekom.bmp.tests.tcs_04_loginAndRegistration;
 
 import com.google.inject.Inject;
+
 import de.telekom.bmp.BmpApplication;
 import de.telekom.bmp.data.Datapool;
 import de.telekom.bmp.data.User;
+import de.telekom.bmp.functional.GoogleMailAccount;
 import de.telekom.bmp.pages.ForgotPasswordPage;
 import de.telekom.bmp.pages.Login;
 import de.telekom.bmp.pages.ResetPasswordPage;
-import de.telekom.bmp.tests.GoogleMailAccount;
 import static de.telekom.testframework.Actions.*;
-import static de.telekom.testframework.Actions.navigateTo;
 import de.telekom.testframework.selenium.Browser;
 import de.telekom.testframework.selenium.annotations.UseWebDriver;
 import static org.hamcrest.Matchers.notNullValue;
+
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -26,8 +27,10 @@ import org.testng.annotations.Test;
 @UseWebDriver
 @Test(groups = {"qcid-3566"})
 public class TC10_Login_forgotten {
-    public static final String HTACCESS_CREDENTIALS = "toon:HullyGully";
+    private static final String HTACCESS_CREDENTIALS = "toon:HullyGully";
 
+    private static final String APP_DOMAIN = "testcloud.bmptest.de";
+    
     @Inject
     Browser browser;
     
@@ -75,7 +78,7 @@ public class TC10_Login_forgotten {
         mailAccount.setUsername(user.email);
         mailAccount.setPassword(GoogleMailAccount.MAIL_PASSWORD);
         
-        String setNewPasswordLink = mailAccount.checkGoogleMailAccountAndExtractConfirmLink();
+        String setNewPasswordLink = mailAccount.checkGoogleMailAccountAndExtractConfirmLink(APP_DOMAIN);
         
         assertThat(setNewPasswordLink, !setNewPasswordLink.equals(""));
         setNewPasswordLink = addHtaccessCredentials(setNewPasswordLink);
