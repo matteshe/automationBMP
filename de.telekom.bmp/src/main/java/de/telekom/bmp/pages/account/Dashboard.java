53cfd402ca1d75437d6610fc39096e5673ed1777
PierreSQS <p.nomo_messina@external.telekom.de>
2014-04-08 15:59:36 +0200

    locators for invite added. Some imports cleaning

--- de.telekom.bmp/src/main/java/de/telekom/bmp/pages/account/Dashboard.java
+++ de.telekom.bmp/src/main/java/de/telekom/bmp/pages/account/Dashboard.java
@@ -1,17 +1,13 @@
 package de.telekom.bmp.pages.account;
 
 import de.telekom.bmp.BmpApplication;
-import de.telekom.bmp.pages.purchase.CreateOrder;
 import de.telekom.testframework.selenium.Page;
 import de.telekom.testframework.selenium.annotations.Path;
 import de.telekom.testframework.selenium.annotations.UseParent;
 import de.telekom.testframework.selenium.controls.*;
-import de.telekom.testframework.selenium.internal.FieldElementLocator;
 import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 /**
@@ -69,7 +65,7 @@ public class Dashboard extends Page {
     
     @FindBy(xpath = ".//button[@name=':submit']")
     @UseParent("singleInvite")
-    public Link singleInviteBtn;
+    public Button singleInviteBtn;
     
     @FindBy(xpath = ".//div[contains(@class, 'headerTop')]//a[contains(@href, '/directories/')]")
     public Link allMembersLnk;
@@ -80,5 +76,11 @@ public class Dashboard extends Page {
     // Invite popup:
     @FindBy(xpath = ".//form[@class = 'addCoWorkersForm']")
     public Form invitePopup;
+    
+    @FindBy(name = "email")
+    public TextField inviteEmailInput;
+    
+    @FindBy(xpath = "//span[@class='feedbackPanelINFO']")
+    public Text inviteSuccessfullTxt;
     
 }
