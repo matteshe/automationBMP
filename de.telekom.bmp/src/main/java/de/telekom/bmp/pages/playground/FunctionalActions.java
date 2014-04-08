ab131beb063e7062f80620d847f91af9eddf62f3
matteshe <mathias.herkt@sqs.com>
2014-04-08 09:14:13 +0200

    use class in package de.telekom.bmp.functional

--- de.telekom.bmp/src/main/java/de/telekom/bmp/pages/playground/FunctionalActions.java
+++ de.telekom.bmp/src/main/java/de/telekom/bmp/pages/playground/FunctionalActions.java
@@ -3,29 +3,31 @@ package de.telekom.bmp.pages.playground;
 import static de.telekom.testframework.Actions.click;
 import static de.telekom.testframework.Actions.navigateTo;
 import static de.telekom.testframework.Actions.set;
+
 import javax.inject.Inject;
 
 /**
- *
+ * 
  * @author Daniel
  */
+@Deprecated
 public class FunctionalActions {
 
-    @Inject
-    PlayLogin login;
+	@Inject
+	PlayLogin login;
 
-    @Inject
-    PlayHeader header;
+	@Inject
+	PlayHeader header;
 
-    public void login(String username, String password) {
-        navigateTo(login);
+	public void login(String username, String password) {
+		navigateTo(login);
 
-        set(login.username, username);
-        set(login.password, password);
-        click(login.signin);
-    }
+		set(login.username, username);
+		set(login.password, password);
+		click(login.signin);
+	}
 
-    public void logout() {
-        click(header.account.logout);
-    }
+	public void logout() {
+		click(header.account.logout);
+	}
 }
