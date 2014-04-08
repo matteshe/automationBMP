351195fa3314bc65e04989d74bdd4ab29c46c6ca
matteshe <mathias.herkt@sqs.com>
2014-04-08 14:16:19 +0200

    test more generic. version of bmp can now be configured in mongodb

--- de.telekom.bmp.tests/src/test/java/de/telekom/bmp/tests/smoketest/T001_Verify_Release_version_in_Sourcecode.java
+++ de.telekom.bmp.tests/src/test/java/de/telekom/bmp/tests/smoketest/T001_Verify_Release_version_in_Sourcecode.java
@@ -5,12 +5,15 @@ package de.telekom.bmp.tests.smoketest;
 
 import static de.telekom.testframework.Actions.navigateTo;
 import static de.telekom.testframework.Assert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 import javax.inject.Inject;
 
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import de.telekom.bmp.data.ApplicationInfo;
+import de.telekom.bmp.data.Datapool;
 import de.telekom.bmp.pages.Home;
 import de.telekom.testframework.annotations.QCId;
 import de.telekom.testframework.selenium.annotations.UseWebDriver;
@@ -22,20 +25,29 @@ import de.telekom.testframework.selenium.annotations.UseWebDriver;
 @UseWebDriver
 @QCId("5509")
 public class T001_Verify_Release_version_in_Sourcecode {
-	public static final String RELEASE_VERSION = "128.4";
+	public static final String RELEASE_VERSION = "138";
 
 	@Inject
 	Home homePage;
 
+	@Inject
+	Datapool db;
+
+	ApplicationInfo ai;
+
 	@BeforeTest
 	public void setup() {
+		ai = db.getDatastore().find(ApplicationInfo.class).get();
+		if (ai == null) {
+			ai = new ApplicationInfo();
+			ai.setVersion(RELEASE_VERSION);
+			db.save(ai);
+		}
 		navigateTo(homePage);
 	}
 
 	@Test
 	public void checkReleaseVersion() {
-		assertThat("version should be " + RELEASE_VERSION,
-				RELEASE_VERSION.equals(homePage.version));
+		assertThat(homePage.version.getValue(), equalTo(ai.getVersion()));
 	}
-
 }
