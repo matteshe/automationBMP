da2e224ca05e7e757d9e1ce9b9eeae8df9587ca1
matteshe <mathias.herkt@sqs.com>
2014-04-08 17:16:10 +0200

    init checkin. new test case to create a user with the role channel admin

---
+++ de.telekom.bmp.tests/src/test/java/de/telekom/bmp/tests/assign_roles/T01_AssignChannelAdminRole.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package de.telekom.bmp.tests.assign_roles;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import de.telekom.testframework.annotations.QCId;
+import de.telekom.testframework.selenium.annotations.UseWebDriver;
+
+/**
+ * @author Mathias Herkt
+ * 
+ */
+@UseWebDriver
+@QCId("5612")
+public class T01_AssignChannelAdminRole {
+
+	@BeforeTest
+	public void setup() {
+
+	}
+
+	@AfterTest
+	public void tearDown() {
+
+	}
+}
