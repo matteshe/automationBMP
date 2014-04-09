905260769e8c281f1059c63367f29af346f17519
matteshe <mathias.herkt@sqs.com>
2014-04-09 15:05:13 +0200

    update locators

--- de.telekom.bmp/src/main/java/de/telekom/bmp/pages/channel/UserPage.java
+++ de.telekom.bmp/src/main/java/de/telekom/bmp/pages/channel/UserPage.java
@@ -6,9 +6,14 @@ package de.telekom.bmp.pages.channel;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 import de.telekom.testframework.selenium.Application;
 import de.telekom.testframework.selenium.Page;
 import de.telekom.testframework.selenium.annotations.Path;
+import de.telekom.testframework.selenium.annotations.UseParent;
+import de.telekom.testframework.selenium.controls.Link;
 
 /**
  * @author Mathias Herkt
@@ -23,4 +28,10 @@ public class UserPage extends Page {
 		super(application);
 	}
 
+	@FindBy(xpath = "//div[@class = 'csrDetail-top']")
+	private WebElement csrDetailParent;
+
+	@FindBy(xpath = "//a[text() = 'Unternehmensdaten']")
+	@UseParent("csrDetailParent")
+	public Link customerDataLnk;
 }
