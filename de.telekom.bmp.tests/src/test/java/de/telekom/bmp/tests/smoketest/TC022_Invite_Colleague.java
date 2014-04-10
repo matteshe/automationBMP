f6b70ee448b87e8333b4e73b83b74d7d265e1576
matteshe <mathias.herkt@sqs.com>
2014-04-10 15:25:05 +0200

    refactor.

--- de.telekom.bmp.tests/src/test/java/de/telekom/bmp/tests/smoketest/TC022_Invite_Colleague.java
+++ de.telekom.bmp.tests/src/test/java/de/telekom/bmp/tests/smoketest/TC022_Invite_Colleague.java
@@ -1,9 +1,8 @@
 package de.telekom.bmp.tests.smoketest;
 
 import static de.telekom.testframework.Actions.*;
-import static de.telekom.testframework.Assert.*;
 import static de.telekom.testframework.selenium.Matchers.*;
-import static org.hamcrest.Matcher.*;
+import static org.hamcrest.Matchers.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -75,52 +74,60 @@ public class TC022_Invite_Colleague {
     @Inject
     Datapool datapool;
 
+    @Inject
+    AccountHandling accHandling;
+    
+    User normalUser;
+    
     @BeforeTest
     public void setup() {
+    	normalUser = datapool.users()
+    			.filter("role", UserRole.USER)
+    			.filter("valid",true)
+    			.field("name").contains("normalUserForInvite").get();
+    	if (normalUser == null) {
+    		Reporter.reportMessage("Create new user for invitation");
+    		normalUser = User.createUser(MAIL_PREFIX);
+    		normalUser.name = "+normalUserForInvite";
+    		accHandling.registerAccount(normalUser);
+    		datapool.save(normalUser);
+    	}
         navigateTo(login);
-
     }
     
     @Test
     public void inviteColleague() {
-    	User normalUser = datapool.users().filter("role", UserRole.USER).filter("valid",true).get();
-    	assertThat("normal user exists.", normalUser != null);
+    	assertThat(normalUser, is(not(nullValue())));
     	
-    	assertThat("user logged out", login.signinBtn.isDisplayed());
+    	assertThat(login.signinBtn, is(displayed()));
     	fa.login(normalUser.email, normalUser.password);
-
     	click(header.settingsMenu.accountLnk);
     	
     	User inviteUser = getInviteUser();
     	    	
     	set(dashboardPage.inviteEmailInput, inviteUser.email);
         click(dashboardPage.singleInviteBtn);
-        assertThat(dashboardPage.inviteSuccessfullTxt.isDisplayed());
+        assertThat(dashboardPage.inviteSuccessfullTxt, is(displayed()));
         
         Actions.navigateTo(home);
-        Assert.waitFor(5, TimeUnit.SECONDS);
         fa.logout();
-        assertThat("user logged out", header.loginBtn.isDisplayed());
-        
+        assertThat(header.loginBtn, is(displayed()));        
         
         gmail.setMailAccount(inviteUser.email);
-        String confirmLink = gmail.checkGoogleMailAccountAndExtractConfirmLink(AccountHandling.APP_DOMAIN, GoogleMailAccount.INVITE);
+        String confirmLink = gmail.checkGoogleMailAccountAndExtractConfirmLink(AccountHandling.APP_DOMAIN, normalUser.name);
         assertThat("confirm link is not empty", !"".equals(confirmLink));
         confirmLink = addHtaccessCredentials(confirmLink);
         
         browser.navigate().to(confirmLink);
-        Actions.waitFor(2, TimeUnit.SECONDS);
-        
         setupAccountInformations(inviteUser);
-        //Assert.verifyThat("invited user is logged in", header.accountMenu.logoutLnk.isDisplayed());
         
         fa.logout();
-        assertThat("user logged out", header.loginBtn.isDisplayed());
+        assertThat(header.loginBtn, is(displayed()));
         
         // check new user
         fa.login(normalUser.email, normalUser.password);
         click(header.settingsMenu.accountLnk);
-        Actions.waitFor(5, TimeUnit.SECONDS);
+        assertThat(dashboardPage.usersLnk, is(displayed()));
         click(dashboardPage.usersLnk);
         click(acctUserPg.searchInput);
         set(acctUserPg.searchInput, inviteUser.name);
@@ -132,6 +139,7 @@ public class TC022_Invite_Colleague {
     }
 
     private void setupAccountInformations(User user) {
+    	assertThat(invitePage.firstNameInput, is(displayed()));
 		set(invitePage.firstNameInput, user.firstName);
 		set(invitePage.lastNameInput, user.name);
 		set(invitePage.passwordInput, user.password);
@@ -155,17 +163,12 @@ public class TC022_Invite_Colleague {
      * @return the user to invite
      */
     private User getInviteUser() {
-    	User inviteUser = null;// = datapool.users().filter("registered", false).filter("valid", true).get();
-    	
-    	if (inviteUser == null) {
-    		Reporter.reportMessage("Create new user for invatation.");
-    		inviteUser = User.createUser(MAIL_PREFIX);
-        	inviteUser.name += "+INVITE";
-        	inviteUser.valid = true;
-        	datapool.save(inviteUser);
-    	} else {
-    		Reporter.reportMessage("Use non registered user for invatation.");
-    	}
+    	User inviteUser = null;
+		Reporter.reportMessage("Create new user for invatation.");
+		inviteUser = User.createUser(MAIL_PREFIX);
+    	inviteUser.name += "+INVITE";
+    	inviteUser.valid = true;
+    	datapool.save(inviteUser);
     	
     	return inviteUser;
     }
