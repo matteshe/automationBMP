7b9a6749a4f385615392823a86104ee3ebbaa86d
PierreSQS <p.nomo_messina@external.telekom.de>
2014-04-10 10:55:36 +0200

    Resuming implementation  for inviting colleagues.

--- de.telekom.bmp.tests/src/test/java/de/telekom/bmp/tests/smoketest/TC022_Invite_Colleague.java
+++ de.telekom.bmp.tests/src/test/java/de/telekom/bmp/tests/smoketest/TC022_Invite_Colleague.java
@@ -4,6 +4,8 @@ import com.google.inject.Inject;
 import de.telekom.bmp.BmpApplication;
 import de.telekom.bmp.data.Datapool;
 import de.telekom.bmp.data.User;
+import de.telekom.bmp.functional.FunctionalActions;
+import de.telekom.bmp.functional.GoogleMailAccount;
 import de.telekom.bmp.pages.Header;
 import de.telekom.bmp.pages.Home;
 import de.telekom.bmp.pages.Login;
@@ -12,9 +14,10 @@ import de.telekom.bmp.pages.account.Dashboard;
 
 import static de.telekom.testframework.Actions.*;
 import de.telekom.testframework.annotations.QCId;
+import de.telekom.testframework.selenium.Browser;
 import static de.telekom.testframework.selenium.Matchers.isCurrentPage;
 import de.telekom.testframework.selenium.annotations.UseWebDriver;
-import java.util.Date;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.testng.Assert.*;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -22,6 +25,11 @@ import org.testng.annotations.Test;
 @QCId("5145")
 @UseWebDriver
 public class TC022_Invite_Colleague {
+
+    private final String MAIL_PREFIX = "mybmptestuser";
+    private final String PIERROT_STD_PWD = "galerien1?";
+    private final String APP_DOMAIN = "testcloud.bmptest.de";
+    private final String HTACCESS_CREDENTIALS="toon:HullyGully";
 
     @Inject
     BmpApplication app;
@@ -44,20 +52,43 @@ public class TC022_Invite_Colleague {
     @Inject
     Datapool datapool;
 
-// Needed user
+    @Inject
+    GoogleMailAccount googleMailAccount;
+    
+    @Inject
+    FunctionalActions fa;
+
+    @Inject
+    Browser browser;
+
+    // the User who invites
     private User user;
+
+    // the User to invite
+    private User userToInvite;
 
     @BeforeTest
     public void setup() {
-        // Login with a normal User
+        // prepare User to invite;
+        // also create a valid and not registered user
+        userToInvite = User.createUser(MAIL_PREFIX);
+        assertThat("User to invite is not null", userToInvite, notNullValue());
+
+//change standard Default values (standard Password=12345!QAY)
+        userToInvite.password = PIERROT_STD_PWD;
+        userToInvite.name = "UserToInvite";
+        userToInvite.firstName = "PierrotSQS";
+//        datapool.save(userToInvite);
+
+// Login with a normal User
         user = datapool.users().field("valid").equal(true)
                 .field("registered").notEqual(false)
-                .field("email").equal("mybmptestuser+normaluser@gmail.com").get();
+                .field("email").equal("mybmptestuser+normaluserwithapps@gmail.com").get();
 
         assertNotNull(user, "cannot find a valid user");
 
 //        user.valid = false;
-        navigateTo(login);
+//        navigateTo(login);
 
     }
 
@@ -65,11 +96,9 @@ public class TC022_Invite_Colleague {
     public void test_022_Invite_Colleague() throws InterruptedException {
 
         try {
-            set(login.usernameInput, user.email);
+            fa.login(user.email, user.password);           
 
-            set(login.passwordInput, user.password);
-
-            click(login.signinBtn);
+ 
 
             click(header.settingsMenu.accountLnk);
 
@@ -77,22 +106,26 @@ public class TC022_Invite_Colleague {
 //            navigateTo(myApps);
 //
 //            assertThat(myApps, currentPage());
-            String emailtoInvite = "mybmptestuser+toInvite" + new Date().getTime() + "@gmail.com";
+            String emailtoInvite = userToInvite.email;
             set(dashboardPage.inviteEmailInput, emailtoInvite);
             click(dashboardPage.singleInviteBtn);
             assertThat(dashboardPage.inviteSuccessfullTxt.isDisplayed());
-            
-            
-            
-
             click(header.accountMenu.logoutLnk);
 
             assertThat(home, isCurrentPage());
 
+            // wait the mail to synchronize
             Thread.sleep(5000);
-            
-            createUserToSave();
-//            user.valid = true;
+
+            googleMailAccount.setMailAccount(emailtoInvite);
+            String setInviteLink = googleMailAccount.checkGoogleMailAccountAndExtractConfirmLink(APP_DOMAIN, GoogleMailAccount.INVITE);
+            setInviteLink = addHtaccessCredentials(setInviteLink);
+
+            browser.navigate().to(setInviteLink);
+
+            Thread.sleep(8000);
+
+            // read email
         } finally {
             user.registered = true;
             datapool.save(user);
@@ -100,8 +133,13 @@ public class TC022_Invite_Colleague {
 
     }
 
+    private String addHtaccessCredentials(String link) {
+        return link.replaceFirst("//", "//" + HTACCESS_CREDENTIALS + "@");
+    }
+
     private void createUserToSave() {
 //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
     }
 
 }
