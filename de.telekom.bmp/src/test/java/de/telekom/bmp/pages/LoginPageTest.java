9021a50a72c6084aeb7c0ab056de33eacb003e3d
matteshe <mathias.herkt@sqs.com>
2014-04-11 10:01:20 +0200

    new test classes for super user page objects

---
+++ de.telekom.bmp/src/test/java/de/telekom/bmp/pages/LoginPageTest.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package de.telekom.bmp.pages;
+
+import static de.telekom.testframework.Assert.verifyThat;
+import static de.telekom.testframework.selenium.Matchers.loaded;
+import static org.hamcrest.Matchers.*;
+import static de.telekom.testframework.selenium.Matchers.*;
+
+import javax.inject.Inject;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import de.telekom.testframework.Actions;
+import de.telekom.testframework.selenium.annotations.UseWebDriver;
+
+/**
+ * Test class to check locators on login page
+ * @author Mathias Herkt
+ *
+ */
+@UseWebDriver
+public class LoginPageTest extends BasePageTest {
+	@Inject
+	Login pg;
+	
+	@BeforeMethod
+	public void setup() {		
+		Actions.navigateTo(pg);
+		verifyThat(pg, is(loaded()));
+	}
+	
+	@Test
+	public void verifySimpleLocators() {
+		verifyThat(pg.usernameInput, exists());
+		verifyThat(pg.passwordInput, exists());
+		verifyThat(pg.signinBtn, exists());
+		verifyThat(pg.signuplink, exists());
+		verifyThat(pg.forgotPasswordLnk, exists());
+	}
+}
---
+++ de.telekom.bmp/src/test/java/de/telekom/bmp/pages/superuser/ReportProcessPageTest.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package de.telekom.bmp.pages.superuser;
+
+import static de.telekom.testframework.Assert.verifyThat;
+import static de.telekom.testframework.selenium.Matchers.*;
+import static org.hamcrest.Matchers.*;
+
+import javax.inject.Inject;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import de.telekom.bmp.pages.BasePageTest;
+import de.telekom.testframework.Actions;
+import de.telekom.testframework.selenium.annotations.UseWebDriver;
+
+/**
+ * Test class to check locators on superuser billing admin page
+ * @author Mathias Herkt
+ *
+ */
+@UseWebDriver
+public class ReportProcessPageTest extends BasePageTest {
+	@Inject
+	ReportProcessPage pg;
+	
+	@BeforeMethod
+	public void setup() {
+		login();
+		Actions.navigateTo(pg);
+		verifyThat(pg, is(loaded()));
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		logout();
+	}
+	
+	@Test
+	public void verifySimpleLocators() {
+		verifyThat(pg.overviewTab, exists());
+		verifyThat(pg.exceptionsTab, exists());
+		verifyThat(pg.reportsTab, exists());
+		verifyThat(pg.billsTab, exists());
+		verifyThat(pg.billingAdminTab, exists());
+		verifyThat(pg.shopSettingsTab, exists());
+		verifyThat(pg.indexTab, exists());
+	}
+}
